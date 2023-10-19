9663f5bedb03f9fa403c62c1d100ad33e98d5fc6
Dannode36 <crafty_dan@outlook.com>
2023-10-19 16:58:45 +1100

    Create Inventory.cs

---
+++ Inventory/Inventory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JumbleLibrary.Inventory
+{
+    public class Inventory<T>
+    {
+        List<T> items;
+
+        public void Add(T item)
+        {
+
+        }
+    }
+}
