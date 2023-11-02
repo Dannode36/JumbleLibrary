349ea3988dbecde878a3d0e68bb6992cec96e942
Dannode36 <crafty_dan@outlook.com>
2023-11-02 21:32:13 +1100

    Update Inventory.cs

--- Inventory/Inventory.cs
+++ Inventory/Inventory.cs
@@ -6,13 +6,30 @@ using System.Threading.Tasks;
 
 namespace JumbleLibrary.Inventory
 {
-    public class Inventory<T>
+    public interface IStorage<T>
+    {
+        public void Add(T item);
+        public void Remove(T item);
+        public void Clear();
+    }
+
+    public class Inventory<T> : IStorage<T>
     {
         List<T> items;
 
         public void Add(T item)
         {
+            throw new NotImplementedException();
+        }
 
+        public void Remove(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
         }
     }
 }
